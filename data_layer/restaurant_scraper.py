d3d9effa6ed4227c9b0d8171aac7e4399465c685
Monsieur-M <raho.mr@gmail.com>
2014-11-05 20:18:13 +0100

    Added handler for scrappers

--- data_layer/restaurant_scraper.py
+++ data_layer/restaurant_scraper.py
@@ -7,6 +7,11 @@
 from __future__ import print_function
 import urllib2
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client=MongoClient('localhost',27017)
+db=client.yelp_db
+users_id=db.users_id
 
 
 def scrap_users(url):
@@ -29,6 +34,12 @@ def scrap_users(url):
         for user in soup.find_all('a', class_='user-display-name'):
             user_hash = user.get('href').split('=')[1]
             user_name = user.get_text('data-hovercard-id')
+
+            if users_id.find({'_id':user_hash}).count() == 0:
+                users_id.insert({'_id':user_hash, 'name':user_name})
+                print ('\n', user_name, 'inserted in users_id db')
+            else:
+                print ('\n', user_name, 'already in users_id db')
             user_hashes[user_hash] = user_name
 
     return user_hashes
@@ -44,6 +55,8 @@ def next(base_url, next_page, num):
     return ''.join([base_url, '?start=', str(next_page*num)])
 
 
+
+#Not needed
 def construct_mongotable(dictionary):
     """
 
--- data_layer/user_scraper.py
+++ data_layer/user_scraper.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
 import urllib2
 from bs4 import BeautifulSoup
 import re
 from time import sleep
 from random import uniform
+
+from pymongo import MongoClient
+
+client=MongoClient('localhost',27017)
+db=client.yelp_db
+users_info=db.user_info
+places=db.places
+
 
 def soup_reviews(user_id,page_start):
     url= 'http://www.yelp.com/user_details_reviews_self?userid=%s&rec_pagestart=%d'%(user_id,page_start)
@@ -16,48 +25,83 @@ def soup_friends(user_id):
     return BeautifulSoup(content)
 
 def sleep_rand():
-      a=uniform(0.20,1)
+      a=uniform(0.1,0.5)
       print "sleeping %.2f seconds" %(a)
       sleep(a)
 
-def get_user(user_id,length=140):
+def get_user(user_id,length=100,city=u'k\xf8benhavn'):
 
+    if users_info.find({'_id':user_id}).count() is not 0:
+        print "user already in db"
+        return
+
+
+    #####Getting the reviews#######
     page_start=0
     reviews=[]
-
-    #Getting the reviews
+    t_review_count=0
+    c_review_count=0
     print 'getting reviews..'
     while True:
         print 'scrapping reviews from page %d' %(page_start/10)
         soup=soup_reviews(user_id,page_start)
+        
         for a in soup.find_all('div',class_='review clearfix'):
             r = {}
-            r['place'] = {}
+            p = {}
+            t_review_count+=1
+ 
             rep={'\n':' ','St':'St ','Ave': 'Ave ','Broadway': 'Broadway ', 'Blvd': 'Blvd ','Rd': 'Rd '}
-            r['place']['address'] = replace_all(a.find('address',class_ = 'smaller').get_text().strip(),rep)
-            r['place']['url'] = 'http://www.yelp.com'+a.find('a').get('href').split('?hrid')[0]
-            r['place']['id'] = a.find('a').get('href').split('=')[1].replace('\n','')
-            r['place']['name'] = a.find('a').get_text().replace('\n','').strip()
-            r['place']['categories'] = [c.get_text('href').replace('\n','').strip() for c in a.find('p').find_all('a')]
-            r['rating'] = float(a.find('div',class_='rating').find('i').get('title')[:3].strip())
-            r['created_at'] = a.find(class_='smaller date').get_text().replace('\n','').strip()
-            r['text'] = a.find('div',class_='review_comment').get_text().strip().replace('\n','')
-            if len(r['text']) >length :
-                r['text']=r['text'][:length]+'...'
-            reviews.append(r)
-            sleep_rand()
+            p['address'] = replace_all(a.find('address',class_ = 'smaller').get_text().strip(),rep)
+            
+            #we only want review from the city!!!!!!!
+            if city in p['address'].lower():
+                c_review_count+=1
+                
+                #We store the place in a separate database
+                p['name'] = a.find('a').get_text().replace('\n','').strip()
+                p['_id'] = a.find('a').get('href').split('=')[1].replace('\n','')
+            
+            
+                #p['url'] = 'http://www.yelp.com'+a.find('a').get('href').split('?hrid')[0]
+                p['price'] = None
+                p['categories'] = [c.get_text('href').replace('\n','').strip() for c in a.find('p').find_all('a')]
+           
+            
+                #We only take the id and the name of the place in the review in order to not overload the user
+                r['place_id'] = p['_id']
+                r['place_name'] = p['name']
+
+                r['rating'] = float(a.find('div',class_='rating').find('i').get('title')[:3].strip())
+                r['created_at'] = a.find(class_='smaller date').get_text().replace('\n','').strip()
+                r['text'] = a.find('div',class_='review_comment').get_text().strip().replace('\n','')
+                if len(r['text']) >length :
+                    r['text']=r['text'][:length]+'...'
+                reviews.append(r)
+            
+                places.insert(p)
+                print p['name'], 'inserted in places db'
+        
+
+        sleep_rand()
         if len(soup.find_all('td',class_='nav-links')) <2:
             break
         page_start+=10
-        sleep_rand()
+
+
+
 
     print 'getting user informations..'
     user={}
-    user['id']=user_id
-    user['reviews_count']=len(reviews)
+    user['_id']=user_id
+
+    user['city_reviews_count']=c_review_count
+    user['total_reviews_count']=t_review_count
     user['reviews']=reviews
 
     rep={'\n':'','\'s Profile':''}
+
+    #user name could also be retrived by the other db
     user['name']= replace_all(soup.find('div',class_='about-connections').find('h1').get_text(),rep).strip()
 
     s=soup.find('div',id='about_user_column')
@@ -76,15 +120,25 @@ def get_user(user_id,length=140):
     user['review_votes']=rv
 
     soup2=soup_friends(user_id)
-    friends=[]
-    for f in soup2.find_all('div',class_='friend_box'):
-        friend={}
-        friend['name']=f.find(class_='user-name').get_text().replace('\n','')
-        friend['id']=f.find('a').get('href').split('=')[1].replace('\n','')
-        friend['location']=f.find(class_='user-location').get_text().replace('\n','')
-        friends.append(friend)
-    user['friends']=friends
-    user['friends_count']=len(friends)
+   
+
+##NEED TO PAGES ALSO WITH FRIENDS...##
+  #  friends=[]
+  #  for f in soup2.find_all('div',class_='friend_box'):
+  #      friend={}
+  #      friend['name']=f.find(class_='user-name').get_text().replace('\n','')
+  #      friend['_id']=f.find('a').get('href').split('=')[1].replace('\n','')
+  #      friend['location']=f.find(class_='user-location').get_text().replace('\n','')
+  #      friends.append(friend)
+  #  user['friends']=friends
+  #  user['friends_count']=len(friends)
+
+    ####ADDING USER IN DB####
+    
+    users_info.insert(user)
+    print user['name'], 'inserted in users_info db'
+    
+    ###
 
     return user
 
