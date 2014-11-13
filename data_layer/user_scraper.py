aa2b56a7ad200d2e416a9868c0a43020078a2c6a
aboye <andboy@gmail.com>
2014-11-13 18:38:55 +0100

    adress ok

--- data_layer/user_scraper.py
+++ data_layer/user_scraper.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import urllib2
 from bs4 import BeautifulSoup
-import re
 from time import sleep
 from random import uniform
 
 
 def soup_reviews(user_id, page_start):
-    url = 'http://www.yelp.com/user_details_reviews_self?userid=%s\
-        &rec_pagestart=%d' % (user_id, page_start)
+    url = 'http://www.yelp.com/user_details_reviews_self?userid=%s'\
+        '&rec_pagestart=%d' % (user_id, page_start)
     content = urllib2.urlopen(url).read()
     return BeautifulSoup(content)
 
@@ -26,104 +25,52 @@ def sleep_rand():
     sleep(a)
 
 
-def get_user(user_id, length=100, city=u'k\xf8benhavn'):
+def scrap_reviews(user_id, city, length=100):
+    """
 
-    # Getting the reviews
+    """
+    restaurants = []
 
-    page_start = 0
-    reviews = []
-    t_review_count = 0
-    c_review_count = 0
-    print 'getting reviews..'
-    while True:
-        print 'scrapping reviews from page %d' % (page_start / 10)
-        soup = soup_reviews(user_id, page_start)
+    soup = soup_reviews(user_id, 0)
 
-        for a in soup.find_all('div', class_='review clearfix'):
-            r = {}
-            p = {}
-            t_review_count += 1
+    page_interval = int(soup.find(
+        'td', class_='range-of-total').get_text().strip().split(' ')[-1])/10
 
-            rep = {'\n': ' ', 'St': 'St ', 'Ave': 'Ave ',
-                   'Broadway': 'Broadway ', 'Blvd': 'Blvd ', 'Rd': 'Rd '}
+    for page in xrange(page_interval):
+        if page is not 0:
+            soup = soup_reviews(user_id, page*10)
 
-            p['address'] = replace_all(a.find('address', class_='smaller')
-                                       .get_text().strip(), rep)
+        for review in soup.find_all('div', class_='review'):
+            address = extract_restaurant_address(review)
+            if 'K\xc3\xb8benhavn' in address[-1]:
+                restaurants.append(address)
 
-            # we only want review from the city!!!!!!!
-            if city in p['address'].lower():
-                c_review_count += 1
-
-                # We store the place in a separate database
-                p['name'] = a.find('a').get_text().replace('\n', '').strip()
-                p['_id'] = a.find(
-                    'a').get('href').split('=')[1].replace('\n', '')
-
-                p['price'] = None
-                p['categories'] = [c.get_text('href').replace('\n', '').strip(
-                ) for c in a.find('p').find_all('a')]
-
-                # We only take the id and the name of the place in the review
-                # in order to not overload the user
-                r['place_id'] = p['_id']
-                r['place_name'] = p['name']
-
-                r['rating'] = float(a.find(
-                    'div', class_='rating').find('i').get('title')[:3].strip())
-                r['created_at'] = a.find(
-                    class_='smaller date').get_text().replace('\n', '').strip()
-                r['text'] = a.find(
-                    'div', class_='review_comment').get_text().strip().replace(
-                        '\n', '')
-                if len(r['text']) > length:
-                    r['text'] = r['text'][:length] + '...'
-                reviews.append(r)
-
-        sleep_rand()
-        if len(soup.find_all('td', class_='nav-links')) < 2:
-            break
-        page_start += 10
-
-    print 'getting user informations..'
-    user = {}
-    user['_id'] = user_id
-
-    user['city_reviews_count'] = c_review_count
-    user['total_reviews_count'] = t_review_count
-    user['reviews'] = reviews
-
-    rep = {'\n': '', ' \'s Profile': ''}
-
-    # user name could also be retrived by the other db
-    user['name'] = replace_all(
-        soup.find('div', class_='about-connections').find(
-            'h1').get_text(), rep).strip()
-
-    s = soup.find('div', id='about_user_column')
-
-    user['location'] = s.find(
-        'div', id='profile_questions').find_all(
-            'p')[0].get_text().replace('\n', '').strip()
-    user['created_at'] = s.find(
-        'div', id='profile_questions').find_all(
-            'p')[1].get_text().replace('\n', '').strip()
-
-    review_votes = s.find(
-        'p', class_='i-review-votes-user_social-wrap').get_text()
-
-    if review_votes is not None:
-        rv = {}
-        digits = map(int, re.findall('\d+', review_votes))
-        rv['useful'], rv['funny'], rv['cool'] = digits[:3]
-        rv['total'] = sum(digits)
-    else:
-        rv['useful'], rv['funny'], rv['cool'], rv['total'] = 0
-    user['review_votes'] = rv
-
-    return user
+    return restaurants
 
 
 def replace_all(text, dic):
     for i, j in dic.iteritems():
         text = text.replace(i, j)
     return text
+
+
+def extract_restaurant_address(soup_snippet):
+    """
+    bs4.element.Tag
+    """
+    name = soup_snippet.find('a', class_='biz-name').get_text()
+
+    soup_snippet_string = str(soup_snippet.find('address'))
+
+    # Deals with all the first addr lines in address string,
+    # last address line is a special case
+    cleaned_addr_list = replace_all(soup_snippet_string,
+                                    {'\n': '',
+                                     '<br>': ',',
+                                     '<address>': ''}
+                                    ).strip().split(',')[:-1]
+
+    return_address_list = [name]
+    return_address_list.extend(cleaned_addr_list)
+
+    return return_address_list
