ea15fb2cee0945e27dd8e0383d9846fe0d61606f
aboye <andboy@gmail.com>
2014-11-19 18:17:34 +0100

    scraper done, some documentation missing

--- data_layer/user_scraper.py
+++ data_layer/user_scraper.py
@@ -5,6 +5,17 @@ import re
 
 
 class UserScraper:
+    """
+    The UserScraper class includes tools for scraping the Yelp users places and
+    the review of those. After class instantiation, run the srap_users method
+    with a list of user ids. Afterwords you will be able to get a list
+    containing dictionaries of places the users have visited and a list
+    containing dictionaries with the users and the reviews.
+
+    Fields:
+        places: List containing dictionaries of places the users have visited
+        users: List containing dictionaries with the users and the reviews.
+    """
 
     def __init__(self):
         self.places = []
@@ -14,7 +25,14 @@ class UserScraper:
 
     def scrap_users(self, city, users_ids):
         """
+        The method has to be invoked to start the scraping of the places and
+        the review of the Yelp users
 
+        Args:
+            city (str): Specify which city the place and reviews has to come
+            from to be scrapped
+            user_ids (str list): A list containing all the users hashcode,
+            which the scraper should extract places and reviews from.
         """
         include_last_page = 1
 
@@ -40,7 +58,12 @@ class UserScraper:
 
     def __scrap_users_places(self, city):
         """
+        Scrap a users places of a specific city. Every time the method scrap a
+        place, it save the soup of places review in an internal dictionary
+        with the user_id as the key.
 
+        Args:
+            city (str): The city the reviews has to come from
         """
         for user, soups in self.__soups.items():
             for soup in soups:
@@ -58,7 +81,9 @@ class UserScraper:
 
     def __scrap_users_reviews(self):
         """
-
+        Scrapping all the soups that is in the internal soups_city
+        dictionnaire, and generating a list of dictionnaire containing
+        reviews and user information.
         """
         for user, reviews in self.__soups_city.items():
             user_reviews = []
@@ -81,12 +106,32 @@ class UserScraper:
                                'review_votes': user_votes})
 
     def soup_reviews(self, user_id, page_start):
+        """
+        Generate a url to the specific user review page
+
+        Args:
+            user_id (str): The hashcode of the user
+            page_start: Which user review site to start at, shold
+            be divisible with ten, to avoid missing som reviews.
+
+        Return:
+            str -- The url to the user review page
+        """
         url = 'http://www.yelp.com/user_details_reviews_self?userid=%s'\
             '&rec_pagestart=%d' % (user_id, page_start)
         content = urllib2.urlopen(url).read()
         return BeautifulSoup(content)
 
     def soup_friends(self, user_id):
+        """
+        Generate a url to the users friends
+
+        Args:
+            user_id (str): The hashcode of the user
+
+        Return:
+            str -- The url to the users friends
+        """
         friends_url = 'http://www.yelp.com/user_details_friends?userid=%s'\
             % (user_id)
         content = urllib2.urlopen(friends_url).read()
@@ -94,17 +139,39 @@ class UserScraper:
 
     def get_number_of_pages(self, soup_snippet):
         """
+        Returns the number of pages on the users review page
 
+        Args:
+            soup_snippet (bs4.element.Tag): bs4 tag containing the review page
+
+        Return:
+            int -- The number of pages
         """
         return int(soup_snippet.find(
             'td', class_='range-of-total').get_text().strip().split(' ')[-1])
 
     def replace_all(self, text, dic):
+        """
+        Replaces all elements in a string
+
+        Args:
+            text (str): The text which the replacement should be invoked at
+            dic (dict): A dictionary where the key specify what should be
+            replaced and the element that specify what should be put in
+            instead.
+
+        Return:
+            str -- The replaces text string
+        """
         for i, j in dic.iteritems():
             text = text.replace(i, j)
         return text
 
     def extract_user_information(self, user_id):
+        """
+        
+
+        """
         location = self.__soups[user_id][0].find(
             'div', id='profile_questions').find_all('p')[0].get_text(
             ).replace('\n', '').strip()
