5e46d2886d4d6281a3089d0164eed8dae354d3ba
aboye <andboy@gmail.com>
2014-11-21 09:08:45 +0100

    remove boiler

--- populate_db.py
+++ populate_db.py
@@ -2,8 +2,6 @@
 
 import ConfigParser
 import data_layer.yelp_json as yelp_api
-import data_layer.restaurant_scraper as place_scraper
-import data_layer.user_scraper as user_scraper
 
 
 def config_api(config_file):
@@ -25,15 +23,3 @@ def config_api(config_file):
         conf_dict['token_secret'])
 
     return yelp_api_obj
-
-
-def run():
-    yelp_api = config_api('conf.ini')
-    result_set = yelp_api.query_api('dinner', 'Copenhagen', 1)
-    url_search_list = [url['url'] for url in result_set]
-    users = place_scraper.scrap_users(url_search_list)
-    users = [i for i, j in users.items()]
-    scraper = user_scraper.UserScraper()
-    scraper.scrap_users(u'københavn', users)
-
-    return [scraper.places, scraper.users]
