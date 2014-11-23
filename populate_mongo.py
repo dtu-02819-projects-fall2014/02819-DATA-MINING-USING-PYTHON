bd8c74693361c9e1cc03c2f5bc2911f8c37079f5
aboye <andboy@gmail.com>
2014-11-23 12:25:45 +0100

    populate with command line args

--- populate_mongo.py
+++ populate_mongo.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import functools
+import sys
 import configure_yelp_api
 import data_layer.restaurant_scraper as place_scraper
 import data_layer.user_scraper as user_scraper
 import data_layer.mongo_handler as mongo_handler
 
 
-def run(port=27017, host='localhost', with_drop=True):
+def run(term, location, search_limit, drop_db=True,
+        port=27017, host='localhost'):
     """
     Populates the specified mongodb
 
+    Args:
+        term (str): Specify which term to lookup, for example dinner
+        location (str): Specify the location for example Copenhagen
+        search_limet (int): How many results to return
+
     Kwargs:
+        drop_db (bool): Default true, will drop db collections
         port (str): Default mongodb port, change to use other port
         host (str): Change to your server
     """
@@ -20,12 +28,12 @@ def run(port=27017, host='localhost', with_drop=True):
     scraper = user_scraper.UserScraper()
     handler = mongo_handler.YelpMongoHandler()
 
-    if with_drop:
+    if drop_db:
         handler.drop_collection('users_id')
         handler.drop_collection('places')
         handler.drop_collection('users_info')
 
-    result_set = yelp_api.query_api('dinner', 'Copenhagen', 20)
+    result_set = yelp_api.query_api(term, location, search_limit)
     url_search_list = [url['url'] for url in result_set]
 
     users = place_scraper.scrap_users(url_search_list)
@@ -42,4 +50,27 @@ def run(port=27017, host='localhost', with_drop=True):
         handler.add_document, collection='users_info'), scraper.users)
 
 if __name__ == '__main__':
-    run()
+    try:
+        term = sys.argv[1]
+        location = sys.argv[2]
+        search_limit = int(sys.argv[3])
+        if len(sys.argv) > 4:
+            drop_db = bool(sys.argv[4])
+        elif len(sys.argv) > 5:
+            port = int(sys.argv[5])
+        elif len(sys.argv) > 6:
+            host = sys.argv[6]
+
+        if len(sys.argv) is 4:
+            run(term, location, search_limit)
+        elif len(sys.argv) is 5:
+            run(term, location, search_limit, drop_db)
+        elif len(sys.argv) is 6:
+            run(term, location, search_limit, drop_db, port)
+        elif len(sys.argv) is 7:
+            run(term, location, search_limit, drop_db, port, host)
+
+    except IndexError:
+        print('Arguments should be: \n term, location, search_limit,'
+              'drop_db (defualt: True), port (default: 27017),'
+              'host (default: localhost)')
