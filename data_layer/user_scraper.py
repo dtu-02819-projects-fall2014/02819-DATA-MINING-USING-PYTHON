591d95147cf85ac34139d65e12a3a5fe12bdaab9
Monsieur-M <raho.mr@gmail.com>
2014-11-27 11:55:26 +0100

    more sleep !

--- data_layer/user_scraper.py
+++ data_layer/user_scraper.py
@@ -85,7 +85,7 @@ class UserScraper:
         """
         Makes the thread sleep a few seconds
         """
-        a = random.uniform(1, 1.5)
+        a = random.uniform(5, 7)
         time.sleep(a)
 
     def __scrap_users_places(self):
