be4b9e0a22e5a041589b2876d475f203d244ce33
aboye <andboy@gmail.com>
2014-11-29 15:14:23 +0100

    unit-test done

--- test.py
+++ test.py
@@ -5,18 +5,42 @@ import os
 import urllib2
 from bs4 import BeautifulSoup
 from data_layer.user_scraper import UserScraper
+import data_layer.restaurant_scraper as restaurant_scraper
 
 # initial setup for testing the UserScraper
 
 # use a downloaded html page to test on
-url_path = os.path.abspath('test_data/yelp_user_test_page.html')
-test_url = 'file://{}'.format(url_path)
-local_test_url = urllib2.urlopen(test_url).read()
+url_path_page = os.path.abspath('test_data/yelp_test_page.html')
+url_path_user = os.path.abspath('test_data/yelp_profile_test_page.html')
+url_path_empty = os.path.abspath('test_data/yelp_empty_user_test.html')
+url_path_no_tag = os.path.abspath('test_data/yelp_user_no_price_cat_tag.html')
+url_path_no_price = os.path.abspath('test_data/yelp_no_price_tag.html')
+url_path_place = os.path.abspath('test_data/yelp_place_test_page.html')
+test_url_page = 'file://{}'.format(url_path_page)
+test_url_user = 'file://{}'.format(url_path_user)
+test_url_empty = 'file://{}'.format(url_path_empty)
+test_url_no_tag = 'file://{}'.format(url_path_no_tag)
+test_url_no_price = 'file://{}'.format(url_path_no_price)
+test_url_place = 'file://{}'.format(url_path_place)
+local_test_url_page = urllib2.urlopen(test_url_page).read()
+local_test_url_user = urllib2.urlopen(test_url_user).read()
+local_test_url_empty = urllib2.urlopen(test_url_empty).read()
+local_test_url_no_tag = urllib2.urlopen(test_url_no_tag).read()
+local_test_url_no_price = urllib2.urlopen(test_url_no_price).read()
+local_test_url_place = urllib2.urlopen(test_url_place).read()
 
-user_test_soup = BeautifulSoup(local_test_url)
+test_soup_page = BeautifulSoup(local_test_url_page)
+test_soup_user = BeautifulSoup(local_test_url_user)
+test_soup_empty = BeautifulSoup(local_test_url_empty)
+test_soup_no_tag = BeautifulSoup(local_test_url_no_tag)
+test_soup_no_price = BeautifulSoup(local_test_url_no_price)
+test_soup_place = BeautifulSoup(local_test_url_place)
 scraper = UserScraper()
+pages_user1 = [test_soup_user]
+pages_user2 = [test_soup_empty]
+scraper.soups = {'test_user': pages_user1, 'empty_user': pages_user2}
 
-# tests
+# Start testing
 
 
 def test_replace_all():
@@ -25,13 +49,23 @@ def test_replace_all():
     assert_equals(scraper.replace_all(test_str, replace_dict), 'abcd')
 
 
+def test_generate_review_url():
+    test_str = u'http://www.yelp.com/user_details_reviews_self?'\
+        u'userid=xW6wTwFRqtM5cV_tJTXrHA&review_filter=location&'\
+        u'location_filter_city=K\xf8benhavn&location_filter_state'\
+        u'=84&rec_pagestart=10'
+    assert_equals(
+        scraper.generate_review_url(
+            'xW6wTwFRqtM5cV_tJTXrHA', 10, u'København', '84'), test_str)
+
+
 def test_extract_address():
-    assert_equals(scraper.extract_address(user_test_soup),
+    assert_equals(scraper.extract_address(test_soup_page),
                   {'address': u'Esplanaden 4'})
 
 
 def test_extract_information():
-    assert_equals(scraper.extract_information(user_test_soup),
+    assert_equals(scraper.extract_information(test_soup_page),
                   {'url': u'http://www.yelp.dk'
                    u'http://www.yelp.dk/biz/toldbod-bodega-k%C3%B8benhavn',
                    '_id': u'cqXYMV1alfHWKsgPBkreLQ',
@@ -39,13 +73,24 @@ def test_extract_information():
 
 
 def test_extract_price_and_category_base_case():
-    assert_equals(scraper.extract_price_and_category(user_test_soup),
+    assert_equals(scraper.extract_price_and_category(test_soup_page),
                   {'price': 2,
                    'categories': [u'Bodegaer', u'Dansk', u'Smørrebrød']})
 
 
+def test_extract_price_and_category_no_tags():
+    assert_equals(scraper.extract_price_and_category(test_soup_no_tag),
+                  {'price': None, 'categories': None})
+
+
+def test_extract_price_and_category_no_price():
+    assert_equals(scraper.extract_price_and_category(test_soup_no_price),
+                  {'price': None,
+                  'categories': [u'Ice Cream & Frozen Yogurt']})
+
+
 def test_extract_rating():
-    assert_equals(scraper.extract_rating(user_test_soup),
+    assert_equals(scraper.extract_rating(test_soup_page),
                   {'created_at': u'19-11-2014',
                    'rating': 3.0, 'text': u'Et fors\xf8g p\xe5 at pr\xf8ve '
                    u'alle sm\xf8rrebr\xf8dsrestauranter i K\xf8benhavn .. (Du'
@@ -53,4 +98,34 @@ def test_extract_rating():
 
 
 def test_get_number_of_pages():
-    assert_equals(scraper.get_number_of_pages(user_test_soup), 5)
+    assert_equals(scraper.get_number_of_pages(test_soup_page), 5)
+
+
+def test_extract_user_information():
+    assert_equals(scraper.extract_user_information('test_user'),
+                  {'created_at': u'March 2014',
+                   'location': u'N\xf8rrebro, K\xf8benhavn, Denmark',
+                   'name': u'Stephanie "Princess" P.'})
+
+
+def test_extract_votes():
+    assert_equals(scraper.extract_votes('test_user'),
+                  {'funny': 44,
+                   'usefull': 283,
+                   'cool': 141})
+
+
+def test_extract_no_votes():
+    assert_equals(scraper.extract_votes('empty_user'),
+                  {'funny': None,
+                   'usefull': None,
+                   'cool': None})
+
+
+def test_get_number_of_pages_place():
+    assert_equals(restaurant_scraper.get_number_of_pages(test_soup_place), 2)
+
+
+def test_next():
+    assert_equals(restaurant_scraper.next('http://www.test.com', 2, 10),
+                  'http://www.test.com?start=20')
